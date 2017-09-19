2afeb57d170ef0b4e4cc11848271cf866d1c17f7
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-09-18 21:18:09 -0300

    Extract generators

--- src/Random.php
+++ src/Random.php
@@ -4,8 +4,8 @@ namespace PragmaRX\Random;
 
 class Random
 {
-    use CharCase;
-    
+    use Generators, CharCase;
+
     const DEFAULT_STRING_SIZE = 16;
 
     const DEFAULT_PATTERN = '[A-Za-z0-9]';
@@ -35,88 +35,6 @@ class Random
         preg_match_all("/$pattern/", $string, $matches);
 
         return implode('', $matches[0]);
-    }
-
-    /**
-     * Generate a random string.
-     *
-     * @return string
-     */
-    protected function generate()
-    {
-        return $this->numeric
-            ? $this->generateNumeric()
-            : $this->generateAlpha();
-    }
-
-    protected function generateInteger()
-    {
-        return random_int($this->getStart(), $this->getEnd());
-    }
-
-    /**
-     * Generate a random string.
-     *
-     * @param \Closure $generator
-     * @return mixed
-     */
-    protected function generateString($generator)
-    {
-        $string = '';
-
-        while (strlen($string) < $size = $this->getSize()) {
-            $string .= $this->extractPattern($generator($size));
-        }
-
-        return $this->trimToExpectedSize($string, $size);
-    }
-
-    /**
-     * Get the alpha generator.
-     *
-     * @return mixed
-     */
-    protected function getAlphaGenerator()
-    {
-        return function ($size) {
-            return random_bytes($size);
-        };
-    }
-
-    /**
-     * Get the alpha generator.
-     *
-     * @return mixed
-     */
-    protected function getNumericGenerator()
-    {
-        return function () {
-            return random_int(0, PHP_INT_MAX);
-        };
-    }
-
-    /**
-     * Generate a random string.
-     *
-     * @return int|string
-     */
-    protected function generateAlpha()
-    {
-        return $this->generateString($this->getAlphaGenerator());
-    }
-
-    /**
-     * Generate a numeric random value.
-     *
-     * @return int|string
-     */
-    protected function generateNumeric()
-    {
-        if (is_null($this->size) && $this->pattern == static::DEFAULT_PATTERN) {
-            return $this->generateInteger();
-        }
-
-        return $this->generateString($this->getNumericGenerator());
     }
 
     /**
